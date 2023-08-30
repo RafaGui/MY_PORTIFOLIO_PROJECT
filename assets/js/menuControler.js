498746cafa17eb4a8695b2a25a706470370f6695
RafaGui <rafaelwilliansgi4@gmail.com>
2023-08-29 21:14:21 -0300

    Adicionado Script para modal menu

---
+++ assets/js/menuControler.js
@@ -0,0 +1,19 @@
+const Menu = document.querySelector('#modalMenu');
+const Controler = document.querySelector('#ModalMenuControler');
+
+Controler.addEventListener('click', () =>{
+
+    const StateAtual = window.getComputedStyle(Menu);
+
+    if (StateAtual.opacity === "1"){
+        console.log('Invisivel');
+        Menu.style.opacity = "0";
+        Menu.style.pointerEvents = "none";
+        Menu.style.marginTop = "-100%";
+    } else if (StateAtual.opacity === "0"){
+        console.log('Visivel');
+        Menu.style.opacity = "1";
+        Menu.style.pointerEvents = "all";
+        Menu.style.marginTop = "0";
+    }
+});
